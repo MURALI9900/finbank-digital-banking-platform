67fbe834211def0971406b7fb4341634def8f332
MURALI9900 <90686104+MURALI9900@users.noreply.github.com>
2026-09-21 13:19:55 +0530

    Add customer not found exception

---
+++ backend/customer-service/src/main/java/com/finbank/customer/exception/CustomerNotFoundException.java
@@ -0,0 +1,8 @@
+package com.finbank.customer.exception;
+
+public class CustomerNotFoundException extends RuntimeException {
+
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
