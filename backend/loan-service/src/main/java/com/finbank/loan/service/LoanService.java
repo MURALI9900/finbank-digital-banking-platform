328d9e4e1692663f351deccf4807f59c6c2ab499
MURALI9900 <90686104+MURALI9900@users.noreply.github.com>
2026-09-21 14:40:24 +0530

    Add loan service: LoanService.java

---
+++ backend/loan-service/src/main/java/com/finbank/loan/service/LoanService.java
@@ -0,0 +1,3 @@
+package com.finbank.loan.service;
+import com.finbank.loan.dto.*; import java.util.List;
+public interface LoanService{LoanResponse apply(CreateLoanRequest r);LoanResponse get(String ref);List<LoanResponse> customerLoans(String customer);LoanResponse decide(String ref,LoanDecisionRequest r);LoanResponse disburse(String ref);RepaymentResponse createRepayment(RepaymentRequest r);RepaymentResponse pay(String ref,PaymentRequest r);List<RepaymentResponse> repayments(String loanRef);}
